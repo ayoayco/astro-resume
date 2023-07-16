7b7259590960426b1d41fcafb2244a7175568a6a
Ayo <ramon.aycojr@gmail.com>
2023-07-16 21:32:03 +0200

    feat: error message when resume() did not find data

--- src/resume.ts
+++ src/resume.ts
@@ -1,3 +1,14 @@
 export function resume<T = any>(id: string): T {
-    return JSON.parse(document.querySelector<HTMLTextAreaElement>(`#${id}`)?.value ?? '')
+    const element = document.querySelector<HTMLTextAreaElement>(`#${id}`);
+
+    if (element?.value)
+        return JSON.parse(element.value)
+        
+    throw Error(`The call resume('${id}') did not find any data.
+    Check that the following are correct:
+    - The Resumable component is used with correct props
+    - "data" prop is not undefined
+    - "${id}" is the "id" of the Resumable component
+    See examples: https://sr.ht/~ayoayco/astro-resume/#usage
+Stack trace: `)
 }
